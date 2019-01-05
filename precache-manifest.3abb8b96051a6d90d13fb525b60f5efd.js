15f20bcd1245e69a8483ab6dc3b16811add09244
Christina Bergstrom <Christina.d.Bergstrom@gmail.com>
2019-01-05 00:49:08 -0500

    Updates

---
+++ precache-manifest.3abb8b96051a6d90d13fb525b60f5efd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/weatherApp/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/weatherApp/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/weatherApp/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "00ee27ed865f22d77c7cd0c1f4290ab9",
+    "url": "/weatherApp/static/media/owfont-regular.00ee27ed.svg"
+  },
+  {
+    "revision": "0ff3e20d12400699242a",
+    "url": "/weatherApp/static/js/runtime~main.0ff3e20d.js"
+  },
+  {
+    "revision": "f730059427a6f6f05247",
+    "url": "/weatherApp/static/js/main.f7300594.chunk.js"
+  },
+  {
+    "revision": "1b3395efc3b2ec8b7f13",
+    "url": "/weatherApp/static/js/1.1b3395ef.chunk.js"
+  },
+  {
+    "revision": "f730059427a6f6f05247",
+    "url": "/weatherApp/static/css/main.6f7c9052.chunk.css"
+  },
+  {
+    "revision": "481f786698050b82ee3aa4f9fd8d12fc",
+    "url": "/weatherApp/index.html"
+  }
+];
