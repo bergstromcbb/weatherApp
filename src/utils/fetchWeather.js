3d958c8694f0268cb6dfe55dd6532529b99857d2
Christina Bergstrom <Christina.d.Bergstrom@gmail.com>
2019-01-05 00:59:00 -0500

    security issues

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 function fetchWeather (location, callback) {
-	axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=92c716ad45b03948aeee52d1ad4d8b42&cnt=5`)
+	axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=92c716ad45b03948aeee52d1ad4d8b42&cnt=5`)
 	.then(function(res) {
 		return callback(res.data);
 	})
